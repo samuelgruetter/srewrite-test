9cf7601f2d375012ca6ce092a9361c31dd34ba83
VladimirNik <vladimir.nikolaev9@gmail.com>
2013-07-28 18:10:07 +0400

    Pattern match example

--- src/main/scala/com/tree/printer/PatternMatchExample.scala
+++ src/main/scala/com/tree/printer/PatternMatchExample.scala
@@ -8,5 +8,18 @@ package com.tree.printer
  * To change this template use File | Settings | File Templates.
  */
 class PatternMatchExample {
+   val (strVal1, strVal2) = ("STRVAL1", "STRVAL2")
 
+  val test: Any = null
+  test match {
+    case Test1(_) => false
+    case Test2(_ :: list) => true
+    case _ => true
+  }
 }
+
+case class Test1(val strParam: String)
+
+case class Test2(val listParam: List[Int])
+
+
