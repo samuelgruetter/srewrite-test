d044b6aa74e2cade559bd30a174a199ab59b83be
VladimirNik <vladimir.nikolaev9@gmail.com>
2013-07-31 23:56:13 +0400

    MultiParam changed

--- src/main/scala/com/tree/printer/MultiParamExample.scala
+++ src/main/scala/com/tree/printer/MultiParamExample.scala
@@ -1,21 +1,14 @@
-package com.tree.printer
-
-class AP
-
-class BP(val x: Int)
-
-class CP(val v: Int, f: Double)
-
-class DP(override val y: Int, f: Double)(x: Int)(t: Double) extends BBB
-
-class EP(x: Int)(z: Int)(implicit q: String, var u: Int) extends DP(x, 5.0)(z)(7.0) with AAA
-
-class FP(implicit x: Int, z: Double) {
-  def test(implicit v: Double, q: Int) = "ff"
+package com.tree.printer {
+  class AP extends scala.AnyRef;
+  class BP (val x: Int) extends scala.AnyRef;
+  class CP (val v: Int, private[this] val f: Double) extends scala.AnyRef;
+  class DP (override val y: Int, private[this] val f: Double) (private[this] val x: Int) (private[this] val t: Double) extends BBB;
+  class EP (private[this] val x: Int) (private[this] val z: Int) (implicit private[this] val q: String, var u: Int) extends DP(x, 5.0)(z)(7.0) with AAA;
+  class FP (implicit private[this] val x: Int, private[this] val z: Double) extends scala.AnyRef {
+    def test(implicit v: Double, q: Int) = "ff"
+    def this(x: Int) = this()(1, 2.0)
+  };
+  abstract trait AAA extends scala.AnyRef;
+  class BBB extends CCC(5);
+  class CCC (val y: Int) extends scala.AnyRef
 }
-
-trait AAA
-
-class BBB extends CCC(5)
-
-class CCC(val y: Int)
