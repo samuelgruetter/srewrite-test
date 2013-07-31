4deb0356222e39fb74bf412b049941c26f995c81
VladimirNik <vladimir.nikolaev9@gmail.com>
2013-07-31 17:17:50 +0400

    TypeVarianceExample modification

--- src/main/scala/com/tree/printer/TypeVarianceExample.scala
+++ src/main/scala/com/tree/printer/TypeVarianceExample.scala
@@ -6,3 +6,6 @@ trait VarianceTest[A, -B, +C] {
 
 trait ParentVarTest[A, -B, +C] extends VarianceTest[A,B,C]
 
+class VarTest[A, -B, +C, D, +E, -F] {
+  def test[A, B, C, D]: B = ().asInstanceOf[B]
+}
