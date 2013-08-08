4089db381329f1b9e1e1066f502e6d1026ea28aa
VladimirNik <vladimir.nikolaev9@gmail.com>
2013-08-09 03:13:19 +0400

    new tests added, including sealed (any ref problem - in SealedExample class)

---
+++ src/main/scala/com/tree/printer/LazyProblem.scala
@@ -0,0 +1,14 @@
+package lazy.issue
+
+class TestLazy {
+  val a = ""
+  lazy val b = ""
+  implicit lazy val c = ""
+  lazy implicit val d = ""
+  def test = {
+    lazy val e = ""
+    implicit val t = ""
+    lazy implicit val h = ""
+    case class InsideTesting
+  }
+}
