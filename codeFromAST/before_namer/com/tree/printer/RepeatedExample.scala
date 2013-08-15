c590610bce295111a70185d4677190a212fd4700
VladimirNik <vladimir.nikolaev9@gmail.com>
2013-08-16 01:11:50 +0400

    codeFromAST folder removed

--- codeFromAST/before_namer/com/tree/printer/RepeatedExample.scala
+++
@@ -1,9 +0,0 @@
-package com.tree.printer {
-  class RepeatedExample extends scala.AnyRef {
-    def <init>() = {
-      super.<init>();
-      ()
-    };
-    def repeatedTest(strs: _root_.scala.<repeated>[String]) = $qmark$qmark$qmark
-  }
-}
