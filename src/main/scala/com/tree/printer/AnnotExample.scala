4b26dcd1dd925a47f3ad12d1946fa50ceab4fe2b
VladimirNik <vladimir.nikolaev9@gmail.com>
2013-08-22 21:26:06 +0400

    testing commit

--- src/main/scala/com/tree/printer/AnnotExample.scala
+++ src/main/scala/com/tree/printer/AnnotExample.scala
@@ -15,7 +15,7 @@ class AnnotExample {
   def testTransient = ()
 
   @deprecated("firstArg", "secondArg")
-  def overTest = "dfff"
+  def overTest = "dffff"
 }
 
 @scala.annotation.implicitNotFound(msg = "The method requires an implicit org.squeryl.KeyedEntityDef in scope")
