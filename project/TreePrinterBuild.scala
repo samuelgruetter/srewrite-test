975fa5cd6f4aee8b62e81c81ca98ba7b51ed2456
VladimirNik <vladimir.nikolaev9@gmail.com>
2013-09-26 01:48:02 +0400

    experimenta TreePrinterBuild.scala

--- project/TreePrinterBuild.scala
+++ project/TreePrinterBuild.scala
@@ -1,5 +1,10 @@
 import sbt._
 import Keys._
+import com.typesafe.sbt.SbtSite.site
+import com.typesafe.sbt.SbtSite.SiteKeys._
+import com.typesafe.sbt.site.JekyllSupport.Jekyll
+import com.typesafe.sbt.SbtGhPages.ghpages
+import com.typesafe.sbt.SbtGit.git
 
 object TreePrinterBuild extends Build {
   val printPlugin = Project(
@@ -10,10 +15,24 @@ object TreePrinterBuild extends Build {
       version := "0.2.0",
       scalaVersion := "2.10.2",
       libraryDependencies ++= Seq(
-        compilerPlugin("org.scala-lang.plugins" %% "printplugin" % "0.2.0" classifier "assembly")
+        compilerPlugin("org.scala-lang.plugins" %% "printplugin" % "0.2.0")
       ),
+      //compilerPlugin("org.scala-lang.plugins" %% "printplugin" % "0.2.0") //classifier "assembly")
+      //scalacOptions += "-P:printplugin:oversrc"
+      scalacOptions += "-P:printplugin:dir-name:myAST",
+      scalacOptions += "-P:printplugin:base-dir:/home/vova"
+  )) settings (websiteSettings: _*)
 
-      scalacOptions += "-P:printplugin:oversrc"
+  lazy val websiteSettings: Seq[Setting[_]] = (
+    site.settings ++
+    ghpages.settings ++
+    //site.includeScaladoc() ++
+    //site.jekyllSupport() ++
+    Seq(
+      git.remoteRepo := "git@github.com:VladimirNik/sprinter.git"
+      //includeFilter in Jekyll := ("*.html" | "*.png" | "*.js" | "*.css" | "CNAME")
+      // the migration guide goes to scaladoc
+      //excludeFilter in Jekyll := ("actors-migration-guide.html")
     )
   )
 }
