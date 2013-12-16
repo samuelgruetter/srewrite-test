430bf69f30d70b0554b3750a56950df03a859fa6
samuelgruetter <samuel.gruetter@epfl.ch>
2013-12-16 15:40:51 +0100

    run plugin with override source option

--- src/main/scala/AutoTuplingTest.scala
+++ src/main/scala/AutoTuplingTest.scala
@@ -9,43 +9,43 @@ object AutoTuplingTest {
   def fooo(t: (Int, Int, Int)): Int = ???
   
   def go(): Unit = {
-    foo(3, 4)
+    foo((3, 4))
   }
   
-  val a = fooo(10, 20, 30)
+  val a = fooo((10, 20, 30))
   
   def test1(): Unit = {
-    f()
+    f(())
   }
   
   def test2(): Unit = {
-    foo(
+    foo((
         /* now look how nicely I wrote this: */ 42,
         /* and another number here: */          43
-    )
+    ))
   }
   
   def test2b: Unit = {
-    println(foo(34, 35))
+    println(foo((34, 35)))
   }
   
-  val b = f()
+  val b = f(())
   
   def test3(): Unit = {
     def test(): Unit = {
-      foo(12, 13) // value discarding at end of method
+      foo((12, 13)) // value discarding at end of method
     }
-    val a = fooo(4, 5, 6)
-    foo(12, 13) // value discarding not at end of method
+    val a = fooo((4, 5, 6))
+    foo((12, 13)) // value discarding not at end of method
     println("hi")
   }
   
   def test4(x: Int, y: Int, func: ((Int, Int)) => Int): Int = {
     val inc = (i: Int) => i+1
-    (func andThen inc)  /*/**/*/ (   /*/**/*/
+    (func andThen inc)(  /*/**/*/ (   /*/**/*/
       x + x*y,
       y + y*x
-    /*no space before ')'*/)+1 // no space after '('
+    /*no space before ')'*/))+1 // no space after '('
   }
   
   def test5() {
