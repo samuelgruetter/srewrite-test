b1c4eee064ba1bba864c7a3c431261207e467cf4
samuelgruetter <samuel.gruetter@epfl.ch>
2013-12-16 15:32:17 +0100

    more autotupling tests

--- src/main/scala/AutoTuplingTest.scala
+++ src/main/scala/AutoTuplingTest.scala
@@ -1,5 +1,5 @@
 
-// auto tupling 
+// auto tupling
 object AutoTuplingTest {
   
   def f(u: Unit): Int = 1
@@ -11,6 +11,8 @@ object AutoTuplingTest {
   def go(): Unit = {
     foo(3, 4)
   }
+  
+  val a = fooo(10, 20, 30)
   
   def test1(): Unit = {
     f()
@@ -27,8 +29,28 @@ object AutoTuplingTest {
     println(foo(34, 35))
   }
   
+  val b = f()
+  
   def test3(): Unit = {
+    def test(): Unit = {
+      foo(12, 13) // value discarding at end of method
+    }
     val a = fooo(4, 5, 6)
+    foo(12, 13) // value discarding not at end of method
+    println("hi")
   }
-
+  
+  def test4(x: Int, y: Int, func: ((Int, Int)) => Int): Int = {
+    val inc = (i: Int) => i+1
+    (func andThen inc)  /*/**/*/ (   /*/**/*/
+      x + x*y,
+      y + y*x
+    /*no space before ')'*/)+1 // no space after '('
+  }
+  
+  def test5() {
+    println(test4(30, 50, foo(_)))
+  }
+  
 }
+
